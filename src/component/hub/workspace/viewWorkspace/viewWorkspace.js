4367055a1a3eba119717b481b578224ebd862765
etilevi <eti@leader.codes>
2021-01-28 15:23:52 +0200

    fix git

--- src/component/hub/workspace/viewWorkspace/viewWorkspace.js
+++ src/component/hub/workspace/viewWorkspace/viewWorkspace.js
@@ -33,10 +33,6 @@ function ViewWorkspace({ user, isConfiguratorOpenWorkspace, workspace, setisConf
 
       <>
 
-<<<<<<< HEAD:src/component/hub/workspace/viewWorkspace/ViewWorkspace/ViewWorkspacelist/viewWorkspacelist.js
-        <Card
-          className="cardWorkspace ml-2 "  >
-=======
         <Card className="cardWorkspace" onClick={() => routeToProject()}
           // <Card className="cardWorkspace" onClick={() => { setisConfiguratorOpenWorkspace() }}
 
@@ -56,7 +52,6 @@ function ViewWorkspace({ user, isConfiguratorOpenWorkspace, workspace, setisConf
 
               : null
           }
->>>>>>> dev:src/component/hub/workspace/viewWorkspace/viewWorkspace.js
           < div className="logoWorkspace"
             style={{ backgroundColor: workspace.color ? workspace.color ? workspace.color : "#F7B500" : "#F7B500" }}>
             {workspace.name[0].toUpperCase()}
