d7136f743b57cc4c6f8529dc93a2a039d1fa105b
yael-levi <yael07163@gmail.com>
2021-02-03 11:48:38 +0200

    3.2

--- src/component/warps/configurator/newConfigurator/new_configurator.js
+++ src/component/warps/configurator/newConfigurator/new_configurator.js
@@ -31,12 +31,24 @@ export default connect(mapStateToProps, mapDispatchToProps)(function NewTasck(pr
                     <img src={require('../../../img/logoLeader.svg')}></img>
                 </div>
                 <ul className="list_config">
-                    <li className="li-back" onClick={(e) => changeBackground(e.target)}>My Workspace</li>
-                    <li onClick={(e) => changeBackground(e.target)}>My Projects</li>
-                    <li onClick={(e) => changeBackground(e.target)}>Goals</li>
-                    <li onClick={(e) => changeBackground(e.target)}>Forms</li>
-                    <li onClick={(e) => changeBackground(e.target)}>Email</li>
-                    <li onClick={(e) => changeBackground(e.target)}>Members</li>
+                    <li className="li-back" onClick={(e) => changeBackground(e.target)}>
+                        <img className="mr-2" src={require('../../../img/mail-open-outline.svg')}></img>
+                       My Workspace</li>
+                    <li onClick={(e) => changeBackground(e.target)}>
+                        <img className="mr-2" src={require('../../../img/bag-check.svg')}></img>
+                        My Projects</li>
+                    <li onClick={(e) => changeBackground(e.target)}>
+                        <img className="mr-2" src={require('../../../img/flag-alt.svg')}></img>
+                        Goals</li>
+                    <li onClick={(e) => changeBackground(e.target)}>
+                        <img className="mr-2" src={require('../../../img/form-line.svg')}></img>
+                        Forms</li>
+                    <li onClick={(e) => changeBackground(e.target)}>
+                        <img className="mr-2" src={require('../../../img/mail-open-outline.svg')}></img>
+                        Email</li>
+                    <li onClick={(e) => changeBackground(e.target)}>
+                        <img className="mr-2" src={require('../../../img/users-solid.svg')}></img>
+                        Members</li>
 
                 </ul>
 
