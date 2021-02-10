a5f4979d42401e892706669602154560ca03a8df
BatShevaLeader <70253759+BatShevaLeader@users.noreply.github.com>
2021-02-10 17:18:27 +0200

    design teams

--- src/component/hub/task/viewTaskByCard/viewTaskByCrad.js
+++ src/component/hub/task/viewTaskByCard/viewTaskByCrad.js
@@ -8,16 +8,21 @@ import { InputGroup, FormControl, Table } from 'react-bootstrap'
 // import './viewTaskByCard.css'
 // import './viewTaskByCrad.css'
 import ViewDetails from '../../viewDetails/viewDetails'
-
+import Animation from '../../animation/animation'
 
 function ViewTaskByCrad(props) {
     const [viewDetails, setViewDetails] = useState(false)
+    const [showchalalit, setShowChalalit] = useState(false)
+
     const showDetails = (event) => {
         setViewDetails(true)
     }
-    const closeDetails=(e)=>{
+    const closeDetails = (e) => {
         setViewDetails(false)
 
+    }
+    function addChalalit() {
+        setShowChalalit(true)
     }
 
     return (
@@ -26,7 +31,7 @@ function ViewTaskByCrad(props) {
             <div className="show-task row mx-5 border-bottom">
                 <label className="check-task ml-4   my-2 p-2 col-3">{props.task.description}
                     <input type="checkbox" />
-                    <span className="checkmark "></span>
+                    <span className="checkmark " onClick={() => addChalalit()}></span>
                 </label>
                 <label className="check-task border-left  p-2 col ">{props.task.status}
                 </label>
@@ -34,12 +39,13 @@ function ViewTaskByCrad(props) {
                 </label>
                 <label className="check-task border-left  p-2 col">{props.task.startDate}
                 </label>
-                <label className="check-task border-left p-2 col "><button  onClick={(e) => showDetails(e)}>view details +</button>
+                <label className="check-task border-left p-2 col "><button onClick={(e) => showDetails(e)}>view details +</button>
                 </label>
-                {viewDetails ? <div onClick={(e)=>closeDetails(e)}><ViewDetails > </ViewDetails></div>
+                {viewDetails ? <div onClick={(e) => closeDetails(e)}><ViewDetails > </ViewDetails></div>
                     : null}
-              
+
             </div>
+            {showchalalit ? <Animation /> : null}
 
         </>
     )
