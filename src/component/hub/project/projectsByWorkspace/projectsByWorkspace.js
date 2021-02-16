05359ca7b4c5b7b187823658b9ffa3ce3b28f5e6
BatShevaLeader <70253759+BatShevaLeader@users.noreply.github.com>
2021-02-16 12:28:48 +0200

    Order of the crud file

--- src/component/hub/project/projectsByWorkspace/projectsByWorkspace.js
+++ src/component/hub/project/projectsByWorkspace/projectsByWorkspace.js
@@ -13,7 +13,9 @@ function ProjectsByWorkspace(props, getAllWorkspaces) {
     let { idWorkspace } = useParams();
     let [flug, setFlug] = useState(false)
     let [allWorkspace, setAllWorkspace] = useState(false)
+
     useEffect(() => {
+        // props.getProjectByWId()
         props.getAllWorkspaces()
         // if (props.worksapces)
         //     if (!flug) {
@@ -21,7 +23,6 @@ function ProjectsByWorkspace(props, getAllWorkspaces) {
         //             alert('/workspace')
         //             props.setProjects(props.workspace.projects)
 
-<<<<<<< HEAD
         //         }
         //         else {
         //             alert('/allWorkspace')
@@ -29,30 +30,11 @@ function ProjectsByWorkspace(props, getAllWorkspaces) {
         //             let allProjects = []
         //             props.workspaces.map((myWorkspace) => allProjects.push(myWorkspace.projects))
         //             props.setProjects(allProjects)
-=======
-    // useEffect(() => {
-    // if (window.performance) {
-    // if (performance.navigation.type == 1) {
-    // alert("This page is reloaded");
-    // { props.getAllWorkspaces() }
-    // }
-    // }
-    useEffect(()=>{
-        props.setProjects(props.workspace.projects)
-    },[])
-    
-
-    // }, []);
-
-    // }, []);
->>>>>>> dev
 
         //         }
         //         setFlug(true)
         //     }
-
-
-    }, [props.worksapces]);
+    }, [props]);
 
 
 
@@ -104,8 +86,7 @@ const mapDispatchToProps = (dispatch) => {
     return {
         getAllWorkspaces: () => dispatch(actions.getAllWorkspacesFromServer()),
         setProjects: (p) => dispatch(actions.setProjects(p)),
-        // getAllWorkspacesFromServer: () => dispatch(actions.getAllWorkspacesFromServer()),
-
+        // getProjectByWId: (id) => dispatch(actions.getProjectByWId),
         getProjectsByWorkspaceId: (idWorkspace) => dispatch(actions.getProjectsByWorkspaceId(idWorkspace))
     }
 }
