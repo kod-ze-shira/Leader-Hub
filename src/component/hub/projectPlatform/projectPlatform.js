4c11e7027de126978244400ad054adfbeeff894f
etilevi <eti@leader.codes>
2021-03-22 23:56:56 +0200

    pull request

--- src/component/hub/projectPlatform/projectPlatform.js
+++ src/component/hub/projectPlatform/projectPlatform.js
@@ -65,7 +65,6 @@ function ProjectPlatform(props) {
     }
     return (
         <>
-
             <div className="mt-4 body container-fluid">
                 <div className="cards">
                     <CardsByProject showToast={(obj) => showToastToDeleteTask(obj)} projectId={props.project._id} flag={props.flag} />
