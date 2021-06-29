1b37b3c1e87ad20c3b0a6d3a5c94c3e39ff57b04
MinaPorush <mina@leader.codes>
2021-06-29 09:16:47 +0300

    scrollbar overview

--- src/component/hubFeature/Overview/Overview.js
+++ src/component/hubFeature/Overview/Overview.js
@@ -54,42 +54,51 @@ function Overview(props) {
 
     return (
         <>
-                <div className='scrollbarOverview container-fluid'>
+            <div className='overview container-fluid '>
 
-                    <div className='row '>
-                        <div className='col-lg-9 col-md-12 mr-3'>
+                <div className='row '>
+                    <div className='col-lg-9 col-md-12 mr-3 scrollOverview'>
 
-                            <div className='container-fluid px-0 '>
-                                <div className='row mb-3 divChartAndMembers'>
-                                    <div className='projectName' >
-                                        <Description></Description>
-                                    </div>
-                                    {refresh ?
-                                        <>
-                                            <Members />
-
-                                            <MyChart />
-                                        </>
-                                        : null}
+                        <div className='container-fluid px-0 '>
+                            <div className='row mb-3 divChartAndMembers'>
+                                <div className='projectName' >
+                                    <Description></Description>
                                 </div>
-                                <div className='row'>
-                                    {refresh ?
-                                        <FilesOfProject />
-                                        : null}
-                                </div>
-                            </div>
-                        </div>
-
-                        <div className='col' style={{ height: '87vh' }}>
-                            <div className='container-fluid px-0 '>
                                 {refresh ?
-                                    <HangoutAndLogs></HangoutAndLogs>
+                                    <>
+                                        <Members />
+                                        <MyChart />
+                                    </>
                                     : null}
                             </div>
+                            <div className='row'>
+                                {refresh ?
+                                    <FilesOfProject />
+                                    : null}
+                            </div>
+
+                            <div className='row d-xs-block d-lg-none'>
+                                <div className='col-12'>
+                                    <div className='container-fluid px-0 '>
+                                        {refresh ?
+                                            <HangoutAndLogs></HangoutAndLogs>
+                                            : null}
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                        {/* <Hangout></Hangout> */}
                     </div>
+
+                    <div className='col d-xs-none d-lg-block scrollOverview ' style={{ height: '87vh' }}>
+                        <div className='container-fluid px-0 '>
+                            {refresh ?
+                                <HangoutAndLogs></HangoutAndLogs>
+                                : null}
+                        </div>
+                    </div>
+                    {/* <Hangout></Hangout> */}
                 </div>
+            </div>
         </>
     )
 }
